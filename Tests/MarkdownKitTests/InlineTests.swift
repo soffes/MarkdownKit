5efdaf01cf02760f246512ffdafbcf8c849f4941
Haldun Bayhantopcu <habayhan@microsoft.com>
2020-10-03 10:56:33 +0200

    addressing PR comment

--- Tests/MarkdownKitTests/InlineTests.swift
+++ Tests/MarkdownKitTests/InlineTests.swift
@@ -190,11 +190,13 @@ final class InlineTests: XCTestCase {
 
     func testMail() {
         let markdown = "<foo@bar.com>"
-        let document = Parser.parse(markdown)!
-        let node = document.children[0].children[0] as! Link
-        XCTAssertEqual(NSRange(location: 0, length: 13), node.range)
-        XCTAssert(node.isAutolink)
-        XCTAssertEqual(node.delimiters, [
+        let document = Parser.parse(markdown)
+        XCTAssertNotNil(document)
+        let node = document!.children[0].children[0] as? Link
+        XCTAssertNotNil(node)
+        XCTAssertEqual(NSRange(location: 0, length: 13), node!.range)
+        XCTAssert(node!.isAutolink)
+        XCTAssertEqual(node!.delimiters, [
             NSRange(location: 0, length: 1), NSRange(location: 12, length: 1)
         ])
     }
